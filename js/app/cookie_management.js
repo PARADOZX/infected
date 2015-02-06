a2ecea8c19544d48de67fcb6944e83244019fa5c
James Ling Chiang <jcc9283@hotmail.com>
2015-02-06 00:51:49 -0500

    add documentation

--- js/app/cookie_management.js
+++ js/app/cookie_management.js
@@ -1,6 +1,8 @@
 define(function (require, exports){
 
+
 	"use strict";
+
 
 	function setCookie(name, value, days)
 	{
@@ -11,12 +13,22 @@ define(function (require, exports){
 		document.cookie = name + '=' + value + '; expires=' + now.toUTCString() + '; path=/';
 	}
 
+
 	function deleteCookie(name)
 	{
 		document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
 		console.log('cookie deleted');
 	}
 
+
+	/**
+	* Checks if 'fbsr' cookie (which value is a escaped/hashed base64-encoded JSON sting containing a payload of data) 
+	* created by Facebook SDK exists.  The cookie is created upon sign in and deleted upon log out.
+	*
+	* @function FBcookieExists
+	* @param {Object} document.cookie array object
+	* @return {Boolean} Returns true if cookie exists.
+	*/
 	function FBcookieExists(a){
 		var a = a.split('; ');
         for(var i=0;i < a.length;i++) {
@@ -25,8 +37,10 @@ define(function (require, exports){
         }
     }
 
+
 	exports.setCookie = setCookie;
 	exports.deleteCookie = deleteCookie;
 	exports.FBcookieExists = FBcookieExists;
+
 
 });
